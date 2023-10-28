9c335b00bbee8cbee3662fe18606e5df8bc8f905
alan-stall <alan_kstall@hotmail.com>
2023-10-28 07:42:48 -0300

    feat: created validators form n save localStorage

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 
 import { FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,17 +10,35 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class CadastroComponent {
   private fb = inject(FormBuilder);
+  user: User = new User();
   addressForm = this.fb.group({
     id: '',
-    firstName: [null, Validators.required],
+    firstName: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+    ],
     email: [null, Validators.required],
     phone: [null, Validators.required],
     password: [null, Validators.required],
   });
 
-  hasUnitNumber = false;
-
   onSubmit(): void {
-    alert('Thanks!');
+    this.user.id = '1';
+    if (this.addressForm.controls['firstName'].value)
+      this.user.firstName = this.addressForm.controls['firstName'].value;
+    if (this.addressForm.controls['email'].value)
+      this.user.email = this.addressForm.controls['email'].value;
+    if (this.addressForm.controls['phone'].value)
+      this.user.phone = this.addressForm.controls['phone'].value;
+    if (this.addressForm.controls['password'].value)
+      this.user.password = this.addressForm.controls['password'].value;
+
+    alert('Você cadastrou!');
+    console.log(this.user);
+    localStorage.setItem('user', JSON.stringify(this.user));
   }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 
 import { FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
 import { AutorizacaoService } from 'src/app/services/autorizacao.service';
 
 @Component({
@@ -11,11 +12,34 @@ import { AutorizacaoService } from 'src/app/services/autorizacao.service';
 export class LoginComponent {
   private fb = inject(FormBuilder);
   addressForm = this.fb.group({
-    email: [null, Validators.required],
-    password: [null, Validators.required],
+    email: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30),
+        Validators.email,
+      ]),
+    ],
+    password: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(12),
+      ]),
+    ],
   });
+  email = this.addressForm.controls['email'];
 
-  hasUnitNumber = false;
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'O email é obrigatório';
+    }
+    return this.email.hasError('email')
+      ? 'Você deve preencher um e-mail válido'
+      : '';
+  }
 
   constructor(
     // private fb: FormBuilder,
