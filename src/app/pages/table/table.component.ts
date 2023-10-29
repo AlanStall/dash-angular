bf5cd1d9f68881df08d071b15651a495c6c890c5
alan-stall <alan_kstall@hotmail.com>
2023-10-29 20:43:15 -0300

    feat: page table with sort n pagination working

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,26 +1,46 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { TableDataSource, TableItem } from './table-datasource';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<TableItem>;
-  dataSource = new TableDataSource();
+  @ViewChild(MatTable, { static: false }) table!: MatTable<User>;
+  dataSource = new MatTableDataSource<User>([]);
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'firstName', 'email', 'phone', 'cpf'];
+
+  constructor(public service: UserService) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.service.getUsers().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.dataSource = new MatTableDataSource<User>(response);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (error: any) => {
+        console.log('Ocorreu algum erro');
+        console.log(error);
+      },
+    });
+  }
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
 }
